e7a4a4c16e15daa2c855d2a4b802c0f813e69f30
ICS_Gabe <29171541+ICSGabe@users.noreply.github.com>
2021-04-04 22:57:31 -0400

    Add files via upload

--- arp_scan.py
+++ arp_scan.py
@@ -1,11 +1,16 @@
 from scapy.all import ARP, Ether, srp
 from mac_vendor_lookup import MacLookup
+import pyfiglet
+
+ascii_banner = pyfiglet.figlet_format("ICS_Gabe")
+print(ascii_banner)
+
 
 #IP Address for the destination subnet. 
 #print("Enter subnet: ") 
 #target_ip = input()
 
-target_ip = input("Enter subnet: ")
+target_ip = input("Enter Subnet or IP Address: ")
 print("Let's see what's lurking on this network :)")
 
 # you can modify this to preset the IP address
@@ -19,7 +24,7 @@ ether = Ether(dst="ff:ff:ff:ff:ff:ff")
 packet = ether/arp
 
 # you can increase timeout value for more accurate results. The srp() method in scapy returns a (sent_packet, received_packet) pair
-result = srp(packet, timeout=10)[0]
+result = srp(packet, timeout=3)[0]
 
 # create an empty list to populate with clients
 clients = []
@@ -31,9 +36,9 @@ try:
 # the MacLookup module throws a KeyError if it does not recognize the mac address of some vendors. go here if you want to more info on the solution: https://github.com/bauerj/mac_vendor_lookup/issues/3
 # this except just keeps the program trucking along when the KeyError rears its ugly head and output Vendor Unknownm for the host
 except KeyError:
-     clients.append({'ip': received.psrc, 'mac': received.hwsrc,'vendors':'Vendor Unknown'})
-    #pass
-
+    clients.append({'ip': received.psrc, 'mac': received.hwsrc,'vendors':'Vendor Unknown'})
+    pass
+    
 print("Available devices in the network:")
 print("IP" + " "*18+"MAC" + " "*18+"VENDOR")
 for client in clients:
